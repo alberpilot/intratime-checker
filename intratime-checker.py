448bc32a159d2341c3ed684e623a65c5ffd71098
jmv74211 <jonathan.martin@wazuh.com>
2019-10-28 23:52:59 +0100

    Added interactive mode in python intratime-checker script

--- intratime-checker.py
+++ intratime-checker.py
@@ -3,19 +3,21 @@
 #  -----------------
 #  @jmv74211
 #  License: GNU General Public License v3.0
-#  Versión 1.0
+#  Versión 1.1
 
 import argparse
 import requests
 import json
 import sys
 import random
+import yaml # PyYAML version 5.1 or greeter due to CVE
 from datetime import datetime
 
 # ---------------------------------------------------------------------------------------------------------------------
 # GLOBAL VARS
 # ---------------------------------------------------------------------------------------------------------------------
 
+CONFIG_FILE = "config.yaml"
 API_URL = "http://newapi.intratime.es"
 API_LOGIN_PATH =  "/api/user/login"
 API_CLOCKING_PATH = "/api/user/clocking"
@@ -28,6 +30,36 @@ API_HEADER = {
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AUXILIARY FUNCTIONS
+# ---------------------------------------------------------------------------------------------------------------------
+
+"""
+    Function to print error messages
+
+    Parameters:
+        - error_message(String): Error message
+"""
+
+def print_error(error_message):
+
+    ERROR_COLOR = '\033[91m'
+
+    print("\n{0} Error: {1}\n".format(ERROR_COLOR,error_message))
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+"""
+    Function to print success messages
+
+    Parameters:
+        - message(String): Success message
+"""
+
+def print_success(message):
+
+    OKGREEN_COLOR = '\033[92m'
+
+    print("\n{0} SUCCESS: {1}\n".format(OKGREEN_COLOR, message))
+
 # ---------------------------------------------------------------------------------------------------------------------
 
 """
@@ -68,19 +100,24 @@ def set_parameters(args):
 
 def check_date_time_format(date, time):
 
+    failed = False
+
     if date != None:
         try:
             datetime.strptime(date, '%Y-%m-%d')
         except ValueError:
-            print("Incorrect data format, should be YYYY-MM-DD")
-            sys.exit(1)
+            print_error("Incorrect data format, should be YYYY-MM-DD")
+            failed = True
 
     if time != None:
         try:
             datetime.strptime(time, '%H:%M:%S')
         except ValueError:
-            print("Incorrect time format, should be HH:MM:SS")
-            sys.exit(1)
+            print_error("Incorrect time format, should be HH:MM:SS")
+            failed = True
+
+    if failed:
+        sys.exit(1)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -88,7 +125,7 @@ def check_date_time_format(date, time):
     Function to transform an action into its respective code
 
     Parameters:
-        - action: example --> ['in, out, pause, return]
+        - action(String): example --> ['in, out, pause, return]
     
     Return
         (int): API action code
@@ -106,7 +143,7 @@ def get_action(action):
     try:
         return switcher[action]
     except:
-        print("ERROR: Invalid action. Choose from 'in', 'out', 'pause', 'return' ")
+        print_error("Invalid action. Choose from 'in', 'out', 'pause', 'return' ")
         sys.exit(1)
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -145,6 +182,72 @@ def get_random_coordinates():
     return wazuh_location_w, wazuh_location_n
 
 # ---------------------------------------------------------------------------------------------------------------------
+
+"""
+    Function to obtain the arguments entered by the user interactively
+    
+    Return
+        (Dictionary): User arguments
+"""
+
+def set_parameters_interactive():
+
+    parameters = {}
+
+    parameters['date'] =  input("Enter the date [YYYY-MM-DD] (default current date): ") 
+    parameters['time'] =  input("Enter the time [HH:MM:SS] (default current time): ")
+    parameters['action'] =  input("Enter the action [in, pause, return, out]: ")
+
+    with open(CONFIG_FILE, 'r') as ymlfile:
+        config_data = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+    parameters['user'] = config_data['authentication']['user']
+    parameters['password'] = config_data['authentication']['password'] 
+
+    if parameters['date'] == '':
+        parameters['date'] =  get_current_date_time().split(" ")[0].strip()
+    
+    if parameters['time'] == '':
+        parameters['time'] =  get_current_date_time().split(" ")[1].strip()
+
+    return parameters
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+"""
+    Function to check and validate the arguments entered by the user interactively
+
+    Parameters:
+        - parameters(Dictionary): User parameters
+"""
+
+def check_parameters_interactive(parameters):
+
+    failed = False
+    allowed_actions = ['in', 'pause', 'return', 'out']
+
+    if parameters['user'] == '' or parameters['user'] == None:
+        print_error("User can not be empty")
+        failed = True
+
+    if parameters['password'] == '' or parameters['password'] == None:
+        print_error("Password can not be empty")
+        failed = True
+
+    if parameters['action'] == '' or parameters['action'] == None:
+        print_error("Action can not be empty")
+        failed = True
+
+    elif parameters['action'] not in allowed_actions:
+        print_error("The action is incorrect: Allowed values {0}".format(allowed_actions))
+        failed = True
+
+    check_date_time_format(parameters['date'], parameters['time'])
+
+    if failed:
+        sys.exit(1)
+
+# ---------------------------------------------------------------------------------------------------------------------
 # MAIN FUNCTIONS
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -168,7 +271,7 @@ def get_login_token(username, password):
         request = requests.post(login_api_url, data=payload, headers=API_HEADER)
         token = json.loads(request.text)['USER_TOKEN']
     except:
-        print("ERROR: Invalid username or password")
+        print_error("Invalid username or password")
         sys.exit(1)
     
     return token
@@ -205,33 +308,46 @@ def clocking(action, token, date=None, time=None):
     try:
         request = requests.post(clocking_api_url, data=payload, headers=API_HEADER)
         if request.status_code == 201:
-            print("Registration successfully: [ action: {0}, date_time: {1}, user_coordinates: ({2},{3}) ]"
+            print_success("Registration successfully: [ action: {0}, date_time: {1}, user_coordinates: ({2},{3}) ]"
                 .format(action, date_time, wazuh_location_w, wazuh_location_n))
         else:
-            print("ERROR: Registration failed, please try again")
+            print_error("Registration failed, please try again")
             sys.exit(1) 
     except:
+        print_error("The request could not be sent to intratime API. Status code = {0}".format(request.status_code))
         raise
-        print("ERROR: The request could not be sent to intratime API. Status code = {0}".format(request.status_code))
-        sys.exit(1)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-u", "--user", type=str, required=True, help="Intratime username.")
-    arg_parser.add_argument("-p", "--password", type=str, required=True, help="Intratime user password")
-    arg_parser.add_argument("-a", "--action", type=str, required=True,  choices=['in','out','pause','return'], 
-                            help="Action")
-    arg_parser.add_argument("-d", "--date", type=str, help="Format YYYY-mm-dd")
-    arg_parser.add_argument("-t", "--time", type=str, help="Format: hh:mm:ss")
-    args = arg_parser.parse_args()
+    # If script mode
+    if len(sys.argv) > 1:
+
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument("-u", "--user", type=str, required=True, help="Intratime username.")
+        arg_parser.add_argument("-p", "--password", type=str, required=True, help="Intratime user password")
+        arg_parser.add_argument("-a", "--action", type=str, required=True,  choices=['in','out','pause','return'], 
+                                help="Action")
+        arg_parser.add_argument("-d", "--date", type=str, help="Format YYYY-mm-dd")
+        arg_parser.add_argument("-t", "--time", type=str, help="Format: hh:mm:ss")
+        args = arg_parser.parse_args()
+        
+        parameters = set_parameters(args)
+
+        check_date_time_format(parameters['date'], parameters['time'])
+
+        token = get_login_token(parameters['user'], parameters['password'])
+
+        clocking(parameters['action'], token, parameters['date'], parameters['time'])
     
-    parameters = set_parameters(args)
+    # If interactive mode
+    else:
+        
+        parameters = set_parameters_interactive()
 
-    check_date_time_format(parameters['date'], parameters['time'])
+        check_parameters_interactive(parameters)
 
-    token = get_login_token(parameters['user'], parameters['password'])
+        token = get_login_token(parameters['user'], parameters['password'])
 
-    clocking(parameters['action'], token, parameters['date'], parameters['time'])
+        clocking(parameters['action'], token, parameters['date'], parameters['time'])
